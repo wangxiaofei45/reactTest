8a906dc1518daf49506de630ffc285d66863308a
gavin <450731224@qq.com>
2019-05-21 16:50:43 +0800

    add

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit src/App.js and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <p className="demo">
+          	这都是什么鬼
+          </p>
+        </header>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
